30641967614826d86b119555ae1efbcf7bc77b5a
sndo9 <robert@sndo9.com>
2016-12-07 14:23:16 -0600

    Work made on High Scores

--- nim/app/src/main/java/com/sndo9/robert/nim/WinScreen.java
+++ nim/app/src/main/java/com/sndo9/robert/nim/WinScreen.java
@@ -1,20 +1,25 @@
 package com.sndo9.robert.nim;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
+import java.util.StringTokenizer;
 
 import static com.sndo9.robert.nim.GameLogic.score;
+import static com.sndo9.robert.nim.SinglePlayer.f;
 import static com.sndo9.robert.nim.SinglePlayer.runningScore;
 
 public class WinScreen extends AppCompatActivity {
@@ -24,15 +29,25 @@ public class WinScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_win_screen);
 
-        HashSet<Tuple> highScore = new HashSet<>();
-
         int runningScore = 0;
         Boolean isOne = false;
         int turns = 0;
+        int i = 0;
         Boolean isAI = false;
 
         String scores;
         String initals;
+        String temp;
+
+        String defaultScore = "0000*0000*0000*0000*0000*0000*0000*0000*0000*0000";
+        String defaultInitials = "000*000*000*000*000*000*000*000*000*000";
+
+        int[] hScores = new int[10];
+        String[] hInitals = new String[10];
+
+        Button playAgain = (Button)findViewById(R.id.buttonPlayAgain);
+
+        SharedPreferences highscores = getSharedPreferences("highscore", 0);
 
         Intent call = getIntent();
 
@@ -53,6 +68,30 @@ public class WinScreen extends AppCompatActivity {
                 if(isOne && isAI) runningScore = runningScore + 1;
 
                 //Check high scores
+                if(!highscores.contains("scores")){
+                    //No previous highscores
+                }
+                else {
+                    //get and parse scores
+                    scores = highscores.getString("scores", defaultScore);
+                    initals = highscores.getString("initials", defaultInitials);
+
+                    StringTokenizer sT = new StringTokenizer(scores, "*");
+                    while(sT.hasMoreTokens()){
+                        hScores[i] = Integer.parseInt(sT.nextToken());
+                        i++;
+                    }
+
+                    i = 0;
+
+                    sT = new StringTokenizer(initals, "*");
+                    while(sT.hasMoreTokens()){
+                        hInitals[i] = sT.nextToken();
+                        i++;
+                    }
+
+                }
+
             }
         }
 
